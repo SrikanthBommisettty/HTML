288bff676f38a5f207d54a08de6cef07946b252d
SrikanthBommisettty <139424151+SrikanthBommisettty@users.noreply.github.com>
2023-10-28 13:51:04 +0530

    Add files via upload

---
+++ await.js
@@ -0,0 +1,12 @@
+async function fetchData() {
+    try {
+      let response = await fetch('https://api.example.com/data');
+      let data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+  
+  fetchData();
+  
