234ff801955d623c041efbabdd76a8c4c20c6b91
SrikanthBommisettty <139424151+SrikanthBommisettty@users.noreply.github.com>
2023-10-30 11:24:26 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,7 @@
+function changeStyle() {
+    document.getElementById("headingElement").textContent="TODAY IS THE FIRST CLASS"
+    document.getElementById("headingElement1").textContent="Classes start from today"
+    document.getElementById("btn").style.display="none"
+    document.getElementById("tittle").style.backgroundColor="green"
+    document.getElementById("headingElement").style.color="red"
+}
