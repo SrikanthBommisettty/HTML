1ff945627bc6df3bd914588e41c0489255699271
SrikanthBommisettty <139424151+SrikanthBommisettty@users.noreply.github.com>
2023-11-08 17:00:22 +0530

    Add files via upload

---
+++ condit.js
@@ -0,0 +1,9 @@
+let num = 15;
+
+if (num > 10) {
+  console.log("Number is greater than 10.");
+} else if (num === 10) {
+  console.log("Number is equal to 10.");
+} else {
+  console.log("Number is less than 10.");
+}
