b5250963650616a4cfda5c477fdfb2de1fda59fa
SrikanthBommisettty <139424151+SrikanthBommisettty@users.noreply.github.com>
2023-12-07 11:51:02 +0530

    Add files via upload

---
+++ objectss.js
@@ -0,0 +1,12 @@
+let user = {
+    name: "srikanth";
+    age: 25;
+    address : {
+        city: "nellore";
+        state: "Andhra Pradesh"
+    };
+    technologies : ["HTML", "CSS", "JS", "REACTJS"]
+}
+console.log(user);
+console.log(user.address);
+console.log(user.technologies);
