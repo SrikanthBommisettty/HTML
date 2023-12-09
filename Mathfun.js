547ff908487297de98129b9a87dc7af3280fcb4c
SrikanthBommisettty <139424151+SrikanthBommisettty@users.noreply.github.com>
2023-12-09 11:53:18 +0530

    Add files via upload

---
+++ Mathfun.js
@@ -0,0 +1,33 @@
+// Math functions in JavaScript
+
+// Math.random(): Returns a random floating-point number between 0 (inclusive) and 1 (exclusive).
+let randomNum = Math.random();
+console.log("Random Number:", randomNum);
+
+// Math.floor(): Rounds a number down to the nearest whole number.
+let roundedDown = Math.floor(8.9);
+console.log("Rounded Down:", roundedDown);
+
+// Math.ceil(): Rounds a number up to the nearest whole number.
+let roundedUp = Math.ceil(8.1);
+console.log("Rounded Up:", roundedUp);
+
+// Math.abs(): Returns the absolute value of a number.
+let absoluteValue = Math.abs(-42);
+console.log("Absolute Value:", absoluteValue);
+
+// Math.pow(): Raises a number to the power of another.
+let powerResult = Math.pow(2, 3);
+console.log("Power Result:", powerResult);
+
+// Math.sqrt(): Returns the square root of a number.
+let squareRoot = Math.sqrt(25);
+console.log("Square Root:", squareRoot);
+
+// Math.max(): Returns the largest of zero or more numbers.
+let maxNumber = Math.max(10, 5, 8, 20);
+console.log("Max Number:", maxNumber);
+
+// Math.min(): Returns the smallest of zero or more numbers.
+let minNumber = Math.min(10, 5, 8, 20);
+console.log("Min Number:", minNumber);
